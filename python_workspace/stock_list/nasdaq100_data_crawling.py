1f33f405edc718278087921f63e891b49c576ef4
flcat <flcat.xd@gmail.com>
2024-05-22 18:03:44 +0900

    오류 수정

--- python_workspace/stock_list/nasdaq100_data_crawling.py
+++ python_workspace/stock_list/nasdaq100_data_crawling.py
@@ -1,78 +1,67 @@
+import datetime
+import json
+import logging
+import os
+import time
+from concurrent.futures import ThreadPoolExecutor
+from os import listdir
+from os.path import isfile, join
+
 import numpy as np
-from pandas_datareader import data as web
 import pandas as pd
 import yfinance as yf
 from dateutil.relativedelta import relativedelta
-import datetime
-import time
-import os
-from os import listdir
-from os.path import isfile, join
-import statsmodels.api as sm
-import seaborn as sns
-from statsmodels.tsa.ar_model import AutoReg, ar_select_order
-import json
-import logging
-from concurrent.futures import ThreadPoolExecutor
+from pandas_datareader import data as web
 
 yf.pdr_override()
-end = datetime.datetime.now()
-stocks_not_downloaded = []
-missing_stocks = []
-folder = '/Users/jaechankwon/Downloads/stock_market/python_workspace/stock_list'
-files = [x for x in listdir(folder) if isfile(join(folder, x))]
-tickers = [os.path.splitext(x)[0] for x in files]
-tickers
-tickers.sort()
-len(tickers)
+
+# 상수 선언
+DATA_FOLDER = '/Users/jaechankwon/python_workspace/stock_list/'
+UPDATE_FOLDER = '/Users/jaechankwon/python_workspace/stock_list/update/'
+SLEEP_TIME = 10
 
 # 로거 설정
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def save_to_csv_from_yahoo(folder, ticker, syear, smonth, sday, eyear, emonth, eday):
-    start = datetime.datetime(syear, smonth, sday)
-    end = datetime.datetime(eyear, emonth, eday)
+def save_to_csv_from_yahoo(folder, ticker, start_date, end_date):
     try:
         logging.info(f"Getting data for {ticker}")
-        df = web.get_data_yahoo(ticker, start, end)
+        df = web.get_data_yahoo(ticker, start_date, end_date)
         if df.empty:
             logging.warning(f"No data for {ticker}")
-            stocks_not_downloaded.append(ticker)
-            return
-        time.sleep(10)
+            return False
+        time.sleep(SLEEP_TIME)
         df.to_csv(f'{folder}/{ticker}.csv')
+        return True
     except Exception as ex:
-        stocks_not_downloaded.append(ticker)
         logging.error(f"Could not get data for {ticker}: {ex}")
+        return False
 
-#Returns a Named Column Data from CSV
 def get_column_from_csv(file, col_name):
     try:
         df = pd.read_csv(file)
         if df.empty:
-            logging.warning(f"No data for {ticker}")
-            stocks_not_downloaded.append(ticker)
-            return
+            logging.warning(f"No data in {file}")
+            return None
     except FileNotFoundError:
-        logging.error("File does not exist")
+        logging.error(f"File {file} does not exist")
+        return None
     else:
         return df[col_name]
 
-# Returns a Named DataFrame from CSV
 def get_df_from_csv(folder, ticker):
+    file_path = f'{folder}/{ticker}.csv'
     try:
-        df = pd.read_csv(f'{folder}/{ticker}.csv', index_col='Date', parse_dates=True)
-    except FileNotFoundError:
-        logging.error(f"File {ticker}.csv doesn't exist")
-    else:
+        df = pd.read_csv(file_path, index_col='Date', parse_dates=True)
         return df
+    except FileNotFoundError:
+        logging.error(f"File {file_path} doesn't exist")
+        return None
 
 def update_stock_data(ticker):
-    up_folder = '/Users/jaechankwon/Downloads/stock_market/python_workspace/stock_list/update/'
-    stock_folder = folder
     try:
-        update_df = get_df_from_csv(up_folder, ticker)
-        orig_df = get_df_from_csv(stock_folder, ticker)
+        update_df = get_df_from_csv(UPDATE_FOLDER, ticker)
+        orig_df = get_df_from_csv(DATA_FOLDER, ticker)
         if orig_df is None or update_df is None:
             return None
         orig_df.drop(orig_df.columns[orig_df.columns.str.contains('unnamed', case=False)],
@@ -85,29 +74,35 @@ def update_stock_data(ticker):
         return None
 
 def save_dataframe_to_csv(df, folder, ticker):
+    file_path = f'{folder}/{ticker}.csv'
     if df is not None:
-        df.to_csv(f'{folder}/{ticker}.csv')
+        df.to_csv(file_path)
     else:
         logging.error(f"Unable to save dataframe for {ticker}")
 
-def update_stocks(stock_folder):
-    for x in tickers:
+def update_stocks():
+    files = [x for x in listdir(DATA_FOLDER) if isfile(join(DATA_FOLDER, x))]
+    tickers = [os.path.splitext(x)[0] for x in files]
+    tickers.sort()
+
+    for ticker in tickers:
         try:
-            logging.info(f"Working on {x}")
-            new_df = update_stock_data(x)
-            save_dataframe_to_csv(new_df, stock_folder, x)
+            logging.info(f"Working on {ticker}")
+            new_df = update_stock_data(ticker)
+            save_dataframe_to_csv(new_df, DATA_FOLDER, ticker)
         except Exception as ex:
             logging.error(f"Error updating stocks: {ex}")
 
-def generate_json_data(folder, tickers):
+def generate_json_data(tickers):
     total_tickers = len(tickers)
     with ThreadPoolExecutor() as executor:
         for i, ticker in enumerate(tickers, start=1):
-            executor.submit(process_ticker, folder, ticker, i, total_tickers)
+            executor.submit(process_ticker, ticker, i, total_tickers)
 
-def process_ticker(folder, ticker, idx, total):
+def process_ticker(ticker, idx, total):
+    file_path = f'{DATA_FOLDER}/{ticker}.csv'
     try:
-        df = pd.read_csv(f'{folder}/{ticker}.csv')
+        df = pd.read_csv(file_path)
         json_data = []
         for _, row in df.iterrows():
             change = row['Close'] - row['Open']
@@ -132,23 +127,27 @@ def process_ticker(folder, ticker, idx, total):
             }
             json_data.append(json_obj)
 
-        with open(f'{folder}/{ticker}.json', 'w') as json_file:
+        with open(f'{DATA_FOLDER}/{ticker}.json', 'w') as json_file:
             json.dump(json_data, json_file, indent=2)
         logging.info(f"[{idx}/{total}] Generated JSON data for {ticker}")
     except FileNotFoundError:
-        logging.error(f"{ticker}.csv file not found")
+        logging.error(f"{file_path} file not found")
     except Exception as ex:
         logging.error(f"Error generating JSON data for {ticker}: {ex}")
 
-update_stocks(folder)
+if __name__ == '__main__':
+    end_date = datetime.datetime.now()
+    start_date = end_date - relativedelta(months=1)
 
-#Defines tickers and folder
-tickers = get_column_from_csv("/Users/jaechankwon/Downloads/stock_market/python_workspace/stock_list/Wilshire-20-Stocks.csv","Ticker")
+    tickers = get_column_from_csv("/Users/jaechankwon/python_workspace/stock_list/Wilshire-20-Stocks.csv", "Ticker")
 
-# companys = ["TSLA", "INTC", "MSFT", "GOOGL", "ADBE", "QCOM", "AAPL", "AMZN", "LRCX", "KLAC", "NVDA", "SBUX", "MAR", "AMD", "NFLX", "LULU", "GOOG", "PYPL", "DASH"]
-for x in range(19):
-    save_to_csv_from_yahoo(folder, tickers[x], 2024, 1, 1, end.year, end.month, end.day)
-logging.info("Finished downloading data")
+if not tickers.empty:
+    for ticker in tickers:
+        save_to_csv_from_yahoo(DATA_FOLDER, ticker, start_date, end_date)
+    logging.info("Finished downloading data")
 
-# JSON 데이터 생성
-generate_json_data(folder, tickers)
+    update_stocks()
+
+    generate_json_data(tickers)
+else:
+    logging.error("Failed to get tickers from CSV file")
